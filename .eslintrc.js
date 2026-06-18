e093b9bce152c4b1e7d5ba89b13af9f715ded935
rbbtsn0w <hamiltonsnow@gmail.com>
2026-06-18 22:44:05 +0800

    chore: change no-explicit-any rule from off to warn per code review feedback

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
   },
   plugins: ['@typescript-eslint'],
   rules: {
-    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
     '@typescript-eslint/no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }],
   },
 };
